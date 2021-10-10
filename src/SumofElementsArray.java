276b23a226773630c25494c3b4e439e602f9fa19
Shivang Sudhakar <shivangsudhakar999@gmail.com>
2021-10-10 07:22:44 +0530

    Sum of elements of array

---
+++ src/SumofElementsArray.java
@@ -0,0 +1,11 @@
+public class SumofElementsArray {
+    public static void main(String[] args) {
+        int A[]={3,9,7,8,12,6,15,5,4,10};
+        int sum=0;
+        for (int i=0;i<A.length;i++){
+            sum=sum+i;
+
+        }
+        System.out.println(sum);
+    }
+}
