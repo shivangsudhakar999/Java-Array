e63d0c8030a25693539b411910c9dc3b010c98c1
Shivang Sudhakar <shivangsudhakar999@gmail.com>
2021-10-10 07:46:20 +0530

    Sum of elements of array

--- src/SumofElementsArray.java
+++ src/SumofElementsArray.java
@@ -3,7 +3,7 @@ public class SumofElementsArray {
         int A[]={3,9,7,8,12,6,15,5,4,10};
         int sum=0;
         for (int i=0;i<A.length;i++){
-            sum=sum+i;
+            sum=sum+A[i];
 
         }
         System.out.println(sum);
